5f60c20b0b600101ae111f42bb85d3d0f7ec0977
Joseph Park <jhspark17@gmail.com>
2020-06-30 21:25:52 -0700

    Add swap function

--- script.js
+++ script.js
@@ -3,25 +3,29 @@ const currencyTwo = document.getElementById("currency-two");
 const amountOne = document.getElementById("amount-one");
 const amountTwo = document.getElementById("amount-two");
 const exchange = document.getElementById("exchange");
-
+const swap = document.getElementById("swap")
 
 function getExchange() {
-  console.log(currencyOne.value)
-  console.log(currencyTwo.value)
-  console.log(amountOne.value);
-  console.log(amountTwo.value);
   fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne.value}`)
   .then(res => res.json())
   .then(data => {
-    exchange.innerHTML = `${data.rates[currencyOne.value]} ${currencyOne.value} = ${data.rates[currencyTwo.value]} ${currencyTwo.value}`;
+    exchange.innerHTML = `${data.rates[currencyOne.value]} ${currencyOne.value}    =    ${data.rates[currencyTwo.value]} ${currencyTwo.value}`;
     amountTwo.value = amountOne.value * data.rates[currencyTwo.value]
   })
 }
 
+function swapValues(){
+  let temp = currencyOne.value;
+  currencyOne.value = currencyTwo.value;
+  currencyTwo.value = temp;
+  getExchange();
+}
 
 
 currencyOne.addEventListener("change", getExchange);
 currencyTwo.addEventListener("change", getExchange);
 amountOne.addEventListener("input", getExchange);
+swap.addEventListener('click', swapValues)
+
 
 
