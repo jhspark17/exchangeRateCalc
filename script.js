68a2de908d0f309fe9e3a0053875a2cb6add109f
Joseph Park <jhspark17@gmail.com>
2020-06-30 18:46:58 -0700

    Add api

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const currencyOne = document.getElementById("currency-one");
+const currencyTwo = document.getElementById("currency-two");
+const exchange = document.getElementById("exchange");
 
-currencyOne.addEventListener("change", (e) => {
-  console.log(e.target.value);
-});
+
+function getExchange() {
+  console.log(currencyOne.value)
+  console.log(currencyTwo.value)
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne.value}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.rates[currencyTwo.value])
+  })
+}
+//`https://api.exchangerate-api.com/v4/latest/${currency_one}`
+
+
+
+currencyOne.addEventListener("change", getExchange);
+currencyTwo.addEventListener("change", getExchange);
+
