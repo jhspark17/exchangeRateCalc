83a012fc75ce462e7621f9dad9df9f0e381d9198
Joseph Park <jhspark17@gmail.com>
2020-06-30 19:05:02 -0700

    Add exchange data

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const currencyOne = document.getElementById("currency-one");
 const currencyTwo = document.getElementById("currency-two");
+const amountOne = document.getElementById("amount-one");
+const amountTwo = document.getElementById("amount-two");
 const exchange = document.getElementById("exchange");
 
 
 function getExchange() {
   console.log(currencyOne.value)
   console.log(currencyTwo.value)
+  console.log(amountOne.value);
+  console.log(amountTwo.value);
   fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne.value}`)
   .then(res => res.json())
   .then(data => {
-    console.log(data.rates[currencyTwo.value])
+    exchange.innerHTML = `${data.rates[currencyOne.value]} ${currencyOne.value} = ${data.rates[currencyTwo.value]} ${currencyTwo.value}`;
+    amountTwo.value = amountOne.value * data.rates[currencyTwo.value]
   })
 }
-//`https://api.exchangerate-api.com/v4/latest/${currency_one}`
 
 
 
 currencyOne.addEventListener("change", getExchange);
 currencyTwo.addEventListener("change", getExchange);
+amountOne.addEventListener("input", getExchange);
+
 
